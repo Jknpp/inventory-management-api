f078dd50a39bfe15dd5fd0665d24587d43637c3a
Jairo Karnopp <122127950+Jknpp@users.noreply.github.com>
2026-08-17 20:52:42 -0300

    feat: add product database migration

---
+++ src/main/resources/db/migration/V2__create_products.sql
@@ -0,0 +1,17 @@
+CREATE TABLE products
+(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(255),
+    sku VARCHAR(100) NOT NULL unique ,
+    price DECIMAL(10,2) NOT NULL,
+    quantity INT NOT NULL,
+    minimum_stock INT ,
+    category_id BIGINT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    CONSTRAINT pk_products PRIMARY KEY (id),
+    CONSTRAINT uk_products_name UNIQUE (name),
+    CONSTRAINT fk_products FOREIGN KEY (category_id) REFERENCES categories(id)
+);
