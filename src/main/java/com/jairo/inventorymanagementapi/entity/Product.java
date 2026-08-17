2ff23f2205cf36d755156181d4ad6138e28fe360
Jairo Karnopp <122127950+Jknpp@users.noreply.github.com>
2026-08-17 20:17:57 -0300

    feat: add product entity

---
+++ src/main/java/com/jairo/inventorymanagementapi/entity/Product.java
@@ -0,0 +1,70 @@
+package com.jairo.inventorymanagementapi.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+@Getter
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String sku;
+
+    private BigDecimal price;
+
+    private int quantity;
+
+    private int minimumStock;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    public Product(String name, String description, String sku, BigDecimal price, int quantity, int minimumStock, Category category) {
+        this.name = name;
+        this.description = description;
+        this.sku = sku;
+        this.price = price;
+        this.quantity = quantity;
+        this.minimumStock = minimumStock;
+        this.category = category;
+    }
+
+    public void update(String name, String description, String sku, BigDecimal price, int minimumStock, Category category ) {
+        this.name = name;
+        this.description = description;
+        this.sku = sku;
+        this.price = price;
+        this.minimumStock = minimumStock;
+        this.category = category;
+    }
+
+    @PrePersist
+    private void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    private void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
